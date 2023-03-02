9ddb74cce19cd3a157ff1cb567cc307ec6cde4ad
Ferdinandy <ferdinandsc@yahoo.com.br>
2023-03-01 21:36:47 -0300

    novas classes

--- core/main.py
+++ core/main.py
@@ -1,1 +1,30 @@
-print('Hello world!')
+
+class Pessoa:
+
+    class Meta:
+        nome=''
+        cpf=''
+        telefone=''
+        endereco=''
+
+class Animal:
+
+    class Meta:
+        tipo=''
+        raca=''
+        porte=''
+        idade=0
+        nome=''
+        peso=1.0
+        vacinas=[]
+
+
+alfredo = Animal()
+alfredo.tipo='cachorro'
+alfredo.raca='Poodle'
+alfredo.idade=8
+alfredo.peso=7
+
+dog=alfredo
+dog.nome='alfredo'
+print(dog.nome)
