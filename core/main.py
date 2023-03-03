7ccfcf06ef3274372f5defe23882ea37e766c1bb
Ferdinandy <ferdinandsc@yahoo.com.br>
2023-03-03 20:29:09 -0300

    petclin update

--- core/main.py
+++ core/main.py
@@ -1,50 +1,54 @@
 class Animal:
 
     def __init__(self):
-        self.nome=''
-        self.idade=0
-        self.raca=''
-        self.porte=''
-        self.vacinas=[]
-        self.historico=[]
-    
+        self.nome = ''
+        self.idade = 0
+        self.raca = ''
+        self.porte = ''
+        self.vacinas = []
+        self.historico = []
+
+
 class Usuario:
 
     def __init__(self):
-        self.nome=''
-        self.telefone=''
-        self.email=''
-        self.cpf=''
-        self.endereco=''
-        self.data_cadastro=''
+        self.nome = ''
+        self.telefone = ''
+        self.email = ''
+        self.cpf = ''
+        self.endereco = ''
+        self.data_cadastro = ''
+
 
 class Cliente(Usuario):
 
     def __init__(self):
-        self.pet=Animal()
+        self.pet = Animal()
+
 
 class Funcionario(Usuario):
 
     def __init__(self):
-        self.salario=0.0
-        self.funcao=''
-        self.horario=''
-        self.escala=''
+        self.salario = 0.0
+        self.funcao = ''
+        self.horario = ''
+        self.escala = ''
+
 
 class Atendimento:
 
     def __init__(self):
-        self.data=''
-        self.hora=''
-        self.animal=Animal()
-        self.tutor=Cliente()
-        self.motivo=''
+        self.data = ''
+        self.hora = ''
+        self.animal = Animal()
+        self.tutor = Cliente()
+        self.motivo = ''
+
 
 class Agenda:
 
-    
     def __init__(self):
-        self.agendamentos=[]
+        self.agendamentos = []
     
     def agendarConsulta(self, cliente, atendimento):
         pass
