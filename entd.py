689799b0bb64f9c178ee92ac835056aada4e97b8
Joalyson-Medeiros <joalysonm@gmail.com>
2023-03-29 21:00:06 -0300

    Criação de classe medicamento

--- entd.py
+++ entd.py
@@ -1,53 +1,58 @@
 class Usuario:
     def __init__(self, nome: str, telefone: str, email: str, cpf: str, endereco: str, data_cadastro: str):
-        self.nome = ''
-        self.telefone = ''
-        self.email = ''
-        self.cpf = ''
-        self.endereco = ''
-        self.data_cadastro = ''
-
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.cpf = cpf
+        self.endereco = endereco
+        self.data_cadastro = data_cadastro
 
 class Funcionario(Usuario):
     def __init__(self, salario: float, funcao: str, horario: str, escala: str, login: str, senha: str, historico_ferias: list):
-        self.salario = 0.0
-        self.funcao = ''
-        self.horario = ''
-        self.escala = ''
-        self.login = ''
-        self.senha = ''
-        self.historico_ferias = []  # list<ferias>
-
+        super().__init__(nome='', telefone="", email="", cpf="", endereco="", data_cadastro="")
+        self.salario = salario
+        self.funcao = funcao
+        self.horario = horario
+        self.escala = escala
+        self.login = login
+        self.senha = senha    
+        self.historico_ferias = historico_ferias  # list<ferias>
 
 class Cliente(Usuario):
     def __init__(self, pets: list, caixa_msgs: list):
-        self.pets = []  # list<animal>
-        self.caixa_msgs = []  # list<string>
-
+        super().__init__(nome='', telefone="", email="", cpf="", endereco="", data_cadastro="")
+        self.pets = pets  # list<animal>
+        self.caixa_msgs = caixa_msgs  # list<string>
 
 class Animal:
     def __init__(self, nome: str, idade: int, raca: str, porte: str, vacinas: list, historico: list):
-        self.nome = ''
-        self.idade = 0
-        self.raca = ''
-        self.porte = ''
-        self.vacinas = []  # list<vacina>
-        self.historico = []  # list<atendimento>
-
+        self.nome = nome
+        self.idade = idade
+        self.raca = raca
+        self.porte = porte
+        self.vacinas = vacinas  # list<vacina>
+        self.historico = historico  # list<atendimento>
 
 class Pagamento:
     def _init_(self, forma: str, parcelamento: int, valor: float):
-        self.forma = ''
-        self.parcelamento = 1
-        self.valor = 0.0
-
+        self.forma = forma
+        self.parcelamento = parcelamento
+        self.valor = valor
 
 class Atendimento:
     def __init__(self, data: str, hora: str, animal: Animal, tutor: Cliente, motivo: str, tipo: str, pagamento: Pagamento):
-        self.data = ''
-        self.hora = ''
-        self.animal = Animal
-        self.tutor = Cliente
-        self.motivo = ''
-        self.tipo = ''
-        self.pagamento = Pagamento()  # classe pagamento
+        self.data = data
+        self.hora = hora
+        self.animal = animal
+        self.tutor = tutor
+        self.motivo = motivo
+        self.tipo = tipo
+        self.pagamento = pagamento  # classe pagamento
+
+class Medicamento:
+    def __init__(self, nome: str, fabricante: str, quantidade: int, validade: str, valor: float):
+        self.nome = nome
+        self.fabricante = fabricante
+        self.quantidade = quantidade
+        self.validade = validade
+        self.valor = valor
