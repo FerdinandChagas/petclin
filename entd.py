a3eb9948cddef763bcdfc0926e0d707bf3e16414
eric santos <eric094@outlook.com>
2023-05-05 20:55:16 -0300

    merge

--- entd.py
+++ entd.py
@@ -1,37 +1,34 @@
 class Usuario:
-    def __init__(self, nome: str, telefone: str, email: str, cpf: str, endereco: str, data_cadastro: str):
+    def __init__(self, nome: str, telefone: str, email: str, cpf: str, endereco: str):
         self.nome = nome
         self.telefone = telefone
         self.email = email
         self.cpf = cpf
         self.endereco = endereco
-        self.data_cadastro = data_cadastro
 
-class Funcionario(Usuario):
-    def __init__(self, salario: float, funcao: str, horario: str, escala: str, login: str, senha: str, historico_ferias: list):
-        super().__init__(nome='', telefone="", email="", cpf="", endereco="", data_cadastro="")
-        self.salario = salario
-        self.funcao = funcao
-        self.horario = horario
-        self.escala = escala
-        self.login = login
-        self.senha = senha    
-        self.historico_ferias = historico_ferias  # list<ferias>
 
 class Cliente(Usuario):
-    def __init__(self, pets: list, caixa_msgs: list):
-        super().__init__(nome='', telefone="", email="", cpf="", endereco="", data_cadastro="")
+    def __init__(self, nome: str, telefone: str, email: str, cpf: str, endereco: str, pets: list):
+        super().__init__(nome, telefone, email, cpf, endereco)
         self.pets = pets  # list<animal>
-        self.caixa_msgs = caixa_msgs  # list<string>
+        self.caixa_msgs = []  # list<string>
+
+    def toString(self):
+        print(f"Nome:{self.nome} telefone:{self.telefone}")
+        print("Animais com vacinas atrasadas:")
+        for animal in self.pets:
+            if len(animal.vacinas) != 0:
+                print(f"Nome do animal: {animal.nome}")
+
 
 class Animal:
-    def __init__(self, nome: str, idade: int, raca: str, porte: str, vacinas: list, historico: list):
+    def __init__(self, nome: str, idade: int, raca: str, porte: str, vacinas: list):
         self.nome = nome
         self.idade = idade
         self.raca = raca
         self.porte = porte
         self.vacinas = vacinas  # list<vacina>
-        self.historico = historico  # list<atendimento>
+
 
 class Pagamento:
     def _init_(self, forma: str, parcelamento: int, valor: float, situacao: bool):
@@ -40,8 +37,10 @@ class Pagamento:
         self.valor = valor
         self.situacao = situacao
 
+
 class Atendimento:
-    def __init__(self, data: str, hora: str, animal: Animal, tutor: Cliente, motivo: str, tipo: str, pagamento: Pagamento, situacao: bool):
+    def __init__(self, data: str, hora: str, animal: Animal, tutor: Cliente, motivo: str, tipo: str,
+                 pagamento: Pagamento, situacao: bool):
         self.data = data
         self.hora = hora
         self.animal = animal
@@ -50,21 +49,25 @@ class Atendimento:
         self.tipo = tipo
         self.pagamento = pagamento  # classe pagamento
         self.situacao = situacao
-   
+
     def __str__(self) -> str:
-        def str_tutor(): 
-            return f"Nome: {self.tutor.nome} | CPF: {self.tutor.cpf} | Email: {self.tutor.email} | "  
+        def str_tutor():
+            return f"Nome: {self.tutor.nome} | CPF: {self.tutor.cpf} | Email: {self.tutor.email} | "
+
         def str_animal():
-            return f"Nome: {self.animal.nome} | Idade: {self.animal.idade} | Raça: {self.animal.raca} | " 
+            return f"Nome: {self.animal.nome} | Idade: {self.animal.idade} | Raça: {self.animal.raca} | "
+
         def str_pagamento():
             return f"Forma: {self.pagamento.forma} | Parcelamento: {self.pagamento.parcelamento} | Valor: {self.pagamento.valor} | "
+
         return \
-        f"data: {self.data} | hora: {self.hora} | " \
-        + f"Motivo: {self.motivo} | Tipo: {self.tipo} | " \
-        + f"Tutor: {str_tutor() if self.tutor else '-'} | " \
-        + f"Animal: {str_animal() if self.animal else '-'} | " \
-        + f"Pagamento: {str_pagamento() if self.pagamento else '-'} | " \
-        + f"Situação: {'Ativo' if not self.situacao else 'Cancelado'}"
+                f"data: {self.data} | hora: {self.hora} | " \
+                + f"Motivo: {self.motivo} | Tipo: {self.tipo} | " \
+                + f"Tutor: {str_tutor() if self.tutor else '-'} | " \
+                + f"Animal: {str_animal() if self.animal else '-'} | " \
+                + f"Pagamento: {str_pagamento() if self.pagamento else '-'} | " \
+                + f"Situação: {'Ativo' if not self.situacao else 'Cancelado'}"
+
 
 class Medicamento:
     def __init__(self, nome: str, fabricante: str, quantidade: int, validade: str, valor: float):
--- main.py
+++ main.py
@@ -1,39 +1,108 @@
 from datetime import datetime
 from agendamento import Agendamento
-from entd import Atendimento
+from entd import Atendimento, Animal, Cliente
+
 
 def render_agendamentos():
 
     controle_agendamento = Agendamento()
+    # Lista de clientes cadastrados no sistema
+    clientes = list()
+    # Lista de Animais cadastrados no sistema
+    animais = list()
 
     while True: 
         print("-------------Menu de Opções de agendamentos-------------")
-        print("-------------1- Realize um Agendamento -------------")
-        print("-------------2 - Listar todos os Agendamentos-------------")
-        print("-------------3 - Cancelar um Agendamento -------------")
-        print("-------------4 - Realize uma prescrição -------------")
-        print("-------------5 - Listar todas as Prescrições -------------")
-        print("-------------          6 - Sair         -------------")
+        print("-------------1 - Realize um Agendamento    -------------")
+        print("-------------2 - Listar todos os Agendamentos-----------")
+        print("-------------3 - Cancelar um Agendamento   -------------")
+        print("-------------4 - Listar clientes e animais -------------")
+        print("-------------5 - Realize uma prescrição    -------------")
+        print("-------------6 - Listar todas as Prescrições -----------")
+        print("-------------7 - Sair                      -------------")
+        print("Digite uma opção: ")
         indice_menu = int(input())
         if indice_menu == 1:
             data = datetime.now()
             hora = f'{data.hour}:{data.minute}:{data.second}'
-            print("informe um motivo")
+            print("informe um motivo: ")
             result_input = input()
-            print("informe um tipo")
+            print("informe um tipo:")
             result_input_tipo = input()
-            atendimento = Atendimento(data=str(data.date()),hora=hora, animal=None, tutor=None, motivo=result_input, tipo=result_input_tipo, pagamento=None, situacao=False)
+            # Recebe os dados do animal
+            nomeAnimal = input("Informe o nome do Animal: ")
+            idadeAnimal = input("Informe a idade do animal: ")
+            racaAnimal = input("Informe a raça do animal: ")
+            porteAnimal = input("Informe o porte do animal(Grande-Medio-Pequeno):")
+            vac = ' '
+            listVac = list()
+
+            # Cria uma entidade animal com os atributos dados
+            animal = Animal(nomeAnimal, idadeAnimal, racaAnimal, porteAnimal, listVac)
+            # Verifica se o animal ja esta cadastrado
+            if len(animais) != 0:
+                cad = False
+                for anim in animais:
+                    if anim.nome == animal.nome and anim.idade == animal.idade and anim.raca == animal.raca:
+                        if anim.porte == animal.porte:
+                            animal = anim
+                            cad = True
+                            break
+                if not cad:
+                    # Caso não esteja vazia, mas o animal nao esteja cadastrado, ele é cadastrado na lista
+                    while vac != 0:
+                        vac = input("Informe a vacina(Digite '0' para terminar de preencher a lista): ")
+                        if vac == "0":
+                            break
+                        listVac.append(vac)
+                    animais.append(animal)
+            else:
+                # Caso esteja vazia termina de preecher os dados do animal e o cadastra na lista
+                while vac != 0:
+                    vac = input("Informe a vacina(Digite '0' para terminar de preencher a lista): ")
+                    if vac == "0":
+                        break
+                    listVac.append(vac)
+                animais.append(animal)
+            # Recebe o CPF
+            cpf = input("Informe o cpf do cliente: ")
+
+            # Verifica se ha cadastros no sistema, e apos se o cpf esta cadastrado no sistema
+            if len(clientes) != 0:
+                for clien in clientes:
+                    if clien.cpf == cpf:
+                        tutor = clien
+            else:
+                # Caso não esteja, entao o cliente sera cadastrado
+                nomeCliente = input("Informe o nome do cliente: ")
+                telefone = input("Informe o telefone: ")
+                email = input("Informe o e-mail do cliente")
+                endereco = input("Informe o endereço do cliente: ")
+                pets = list()
+                pets.append(animal)
+                tutor = Cliente(nomeCliente, telefone, email, cpf, endereco, pets)
+                clientes.append(tutor)
+
+            atendimento = Atendimento(data=str(data.date()), hora=hora, animal=animal, tutor=tutor, motivo=result_input, tipo=result_input_tipo, pagamento=None, situacao=False)
             controle_agendamento.agendarConsulta(atendimento)
         elif indice_menu == 2:
             print([str(a) for a in controle_agendamento.listarAgendamentos()])
         elif indice_menu == 3:
-            controle_agendamento.cancelarConsulta()
-            #Falta as opções do controle de usuario pra buscarmos o cliente
+            cpf = input("Digite o cpf do cliente: ")
+            nomeAnimal = input("Digite o nome do animal")
+            confirmacao = input("Deseja realmente cancelar o agendamento?(s/n)")
+            if confirmacao == "s":
+                controle_agendamento.cancelarConsulta(cpf, nomeAnimal)
+            else:
+                print("O agendamento ainda esta disponivel!")
         elif indice_menu == 4:
-            controle_agendamento.adicionarPrescicao()
+            for cliente in clientes:
+                cliente.toString()
         elif indice_menu == 5:
-            controle_agendamento.obterPrescricoes()
+            controle_agendamento.adicionarPrescicao()
         elif indice_menu == 6:
+            controle_agendamento.obterPrescricoes()
+        elif indice_menu == 7:
             break
  
 render_agendamentos()
